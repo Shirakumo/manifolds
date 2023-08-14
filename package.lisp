da4404edeea68960071b8e177a4619591cbeb777
Shinmera <shinmera@tymoon.eu>
2023-08-14 10:29:09 +0200

    Exports

--- package.lisp
+++ package.lisp
@@ -1,17 +1,27 @@
 (defpackage #:org.shirakumo.fraf.manifolds
   (:use #:cl #:org.shirakumo.fraf.math)
   (:export
+   #:u32
+   #:f32
+   #:f64
    #:do-faces
    #:vertex-adjacency-list
+   #:face-adjacency-list
    #:half-edge-list
    #:edge-list
+   #:boundary-list
+   #:v
    #:face-normal
    #:face-normals
+   #:face-area
+   #:surface-area
+   #:boundary-length
+   #:centroid
+   #:convex-volume
    #:closest-point-on-triangle
    #:face-in-volume-p
    #:faces-in-volume
    #:bounding-box
    #:vertex-faces
    #:2-manifold-p
-   #:separate-meshes
-   #:manifold))
+   #:separate-meshes))
